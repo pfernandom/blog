8d58922d9db99bbf399d4eb83df4d0abecb5d433
Pedro F Marquez <pfernandom@gmail.com>
2024-11-19 11:49:04 -0600

    ActivityPub - Make blog discoverable

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1>About me...</h1>
+      <p>
+        I am a full-stack software developer with a Master of Science in
+        Computer Science and Machine Learning. I have more than 10 years of
+        professional experience in multiple roles that cover application
+        security, back-end, front-end development, and infrastructure
+        development. I currently work as a full-stack engineer at LinkedIn.
+      </p>
+    </div>
+  )
+}
+
+export default About
